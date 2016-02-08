8be64bb73b8e2d063f2eaa07eaacbea42558bbcc
Gabriel <gabriel.bira95@gmail.com>
2016-02-08 23:51:01 +0000

    added some more structure

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+'use strict';
 var express = require('express');
 var mongoose= require('mongoose');
-var routes = require('./app/routes/routes.js');
+var config = require('./app/app.js');
 var app = express();
+var passport = require('passport');
+var expressSession = require('express-session');
 
 require('dotenv').load();
 mongoose.connect(process.env.MONGO_URI);
 
-// routes(app);
 
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+config(app);
+
+
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
