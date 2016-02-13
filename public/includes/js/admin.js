5512f51d7d2dc91df77f3cba2e9fd6f0a0993d23
Gabriel <gabriel.bira95@gmail.com>
2016-02-13 20:51:58 +0000

    Got a user table to work in the admin view

--- public/includes/js/admin.js
+++ public/includes/js/admin.js
@@ -1,3 +1,12 @@
 $('.table-container').dataTable({
-    ajax : 'https://jsu-evals-app-thegame2500.c9users.io/getUsers'
+    "ajax": {
+        "url": "https://jsu-evals-app-thegame2500.c9users.io/getUsers",
+        "dataSrc": ""
+    },
+    processing : true,
+    "columns": [
+            { "data": "user_name" },
+            { "data": "user_password" },
+            { "data": "user_role_id" }
+    ]
 })
