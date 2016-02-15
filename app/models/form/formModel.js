e92ab9654ed8db6289d520b52b57940a63f71f0d
Gabriel <gabriel.bira95@gmail.com>
2016-02-15 22:26:36 +0000

    Refactored that gargantuan model and implemented a rough TO DO list in the read me to keep track of stuff

--- app/models/form/formModel.js
+++ app/models/form/formModel.js
@@ -1,53 +1,82 @@
 var mongoose = require('mongoose');
-var formSchema = new mongoose.Schema({
-    
-    //personal data
+
+var personalDataSchema = new mongoose.Schema({
+    form_id    : {type : mongoose.Schema.Types.ObjectId, ref : FormModel},
     first_name : String,
     age        : Number,
     gender     : String,
     nationality: String,
     adress     : String,
     birth_date : String,
-    CNP        : String,
-    
-    //contact data
+    CNP        : String
+})
+
+var contactDataSchema = new mongoose.Schema({
+    form_id    : {type : mongoose.Schema.Types.ObjectId, ref : FormModel},
     phone      : String,
     mail       : String,
-    facebook   : String,
-    
-    //education
+    facebook   : String
+})
+
+var educationSchema = new mongoose.Schema({
+    form_id    : {type : mongoose.Schema.Types.ObjectId, ref : FormModel},
     highschool : String,
     city       : String,
     county     : String,
     profile    : String,
-    study_lang : String,
-    
-    //parents
+    study_lang : String
+})
+
+var parentsDataSchema = new mongoose.Schema({
+    form_id      : {type : mongoose.Schema.Types.ObjectId, ref : FormModel},
     first_name_p : String,
     last_name_p  : String,
     mail_p       : String,
-    phone_p      : String,
-    
-    //picked faculties
+    phone_p      : String
+})
+
+var facultiesSchema = new mongoose.Schema({
+    form_id      : {type : mongoose.Schema.Types.ObjectId, ref : FormModel},
     faculty_1    : String,
     faculty_2    : String,
-    faculty_3    : String,
-    
-    //questions
+    faculty_3    : String
+})
+
+var questionsSchema = new mongoose.Schema({
+    form_id                : {type : mongoose.Schema.Types.ObjectId, ref : FormModel},
     why_our_uni            : String,
     why_these_faculties    : String,
     why_should_we_pick_you : String,
     why_our_summer_school  : String,
-    cool_things_you_did    : String,
-    
-    //details
+    cool_things_you_did    : String
+})
+
+var detailsSchema = new mongoose.Schema({
+    form_id            : {type : mongoose.Schema.Types.ObjectId, ref : FormModel},
     how_do_you_know_us : String,
     shirt_size         : String,
-    special_diet       : String,
-    
-    //paper scans (jpeg's)
+    special_diet       : String
+})
+
+var picsSchema = new mongoose.Schema({
+    form_id            : {type : mongoose.Schema.Types.ObjectId, ref : FormModel},
     ID                 : { data: Buffer , contentType : String },
     teacher_rec        : { data: Buffer , contentType : String },
     parental_agreement : { data: Buffer , contentType : String }
-    
 })
+
+var formSchema = new mongoose.Schema({
+   _id : mongoose.Schema.Types.ObjectId
+})
+
+var PersonalData  = mongoose.model('PersonalData',personalDataSchema),
+    ContactData   = mongoose.model('ContactData',contactDataSchema),
+    EducationData = mongoose.model('EducationData',educationSchema),
+    ParentsData   = mongoose.model('ParentsData',parentsDataSchema),
+    FacultyData   = mongoose.model('FacultyData',facultiesSchema),
+    QuestionData  = mongoose.model('QuestionData',questionsSchema),
+    DetailsData   = mongoose.model('DetailsData',detailsSchema),
+    PicsData      = mongoose.model('PicsData',picsSchema),
+    FormModel     = mongoose.model('FormModel',formSchema);
+
+module.exports = FormModel;
